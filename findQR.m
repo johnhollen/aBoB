f926781dd974fba2ec25d79f0c23b07f174b97b1
John Hollén <johnhollen982@gmail.com>
2014-11-04 11:08:59 +0100

    Added some more skeleton code

---
+++ findQR.m
@@ -0,0 +1,6 @@
+function [qrImage] = findQR(image)
+%Function for finding and extracting the QR-code
+
+
+
+qrImage = zeros(200);
--- main.m
+++ main.m
@@ -1,8 +1,5 @@
 % Main file for testing tnm034 function 
 
-
 img = imread('testimages/Images_Training_2/Hus_1a.png');
 
-size(img(:,:,3));
-
-imshow(img);
+strOut = tnm034(img);
