78c75e27674fe120c907ae8177a25c1c7fc82235
John Hollén <johnhollen982@gmail.com>
2014-11-06 13:11:52 +0100

    Minor

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 % Main file for testing tnm034 function 
 
-img = imread('testimages/Images_Training_2/Hus_1a.png');
+img = imread('testimages/Images_Training_2/Hus_1e.png');
 %imshow(img);
 %figure
 strOut = tnm034(img);
