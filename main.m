54487dbe3b926faddda52fdc0e73ead996299247
simbe109 <simbe109@student.liu.se>
2014-11-14 13:38:01 +0100

    Completed decodeQR hopefully

--- main.m
+++ main.m
@@ -1,13 +1,14 @@
 % Main file for testing tnm034 function 2
-img = imread('testimages/Images_Training_1/Bygg_1.png');
+%img = imread('testimages/Images_Training_1/Bygg_1.png');
 %img1 = imread('testimages/Images_Training_1/Bygg_2.png');
 %img2= imread('testimages/Images_Training_1/Bygg_3.png');
 %img3 = imread('testimages/Images_Training_1/Bygg_4.png');
+img = imread('testBild2.png');
 
 strOut = tnm034(img);
 %strOut = tnm034(img1);
 %strOut = tnm034(img2);
 %strOut = tnm034(img3);
-
+strOut
 
 
